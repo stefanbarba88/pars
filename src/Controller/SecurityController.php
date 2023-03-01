5b863753696d6c4e6106f13f6cb9618b7dc38199
stefan <stefanmaksimovic88@gmail.com>
2023-03-01 13:24:38 +0100

    user entity

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController {
+
+  #[Route('/login', name: 'app_login')]
+  public function login(AuthenticationUtils $authenticationUtils): Response {
+    $args = [];
+
+    $error = $authenticationUtils->getLastAuthenticationError();
+
+    // last username entered by the user
+    $lastUsername = $authenticationUtils->getLastUsername();
+
+    $args['error'] = $error;
+    $args['lastUsername'] = $lastUsername;
+
+    return $this->render('login.html.twig', $args);
+  }
+
+  #[Route('/logout', name: 'app_logout', methods: 'GET')]
+  public function logout(): Response {
+    return new Response('logout');
+  }
+
+}

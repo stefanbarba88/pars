7f9711f4b47c3fa083690c8f20fcc8a0f1f4dd40
stefan <stefanmaksimovic88@gmail.com>
2023-03-02 14:45:12 +0100

    home controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,14 +8,14 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-//#[IsGranted('ROLE_USER', message: 'Nemas pristup', statusCode: 403)]
+#[IsGranted('ROLE_USER', message: 'Nemas pristup', statusCode: 403)]
 
 class HomeController extends AbstractController {
   #[Route('/', name: 'app_home')]
   public function index(): Response {
-    if (!$this->isGranted('ROLE_USER')) {
-      return $this->redirectToRoute('app_login');
-    }
+//    if (!$this->isGranted('ROLE_USER')) {
+//      return $this->redirectToRoute('app_login');
+//    }
 
     return $this->render('home/index.html.twig', [
       'controller_name' => 'HomeController',
